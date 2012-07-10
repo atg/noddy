b7644c9a1df624c2e1c4a7a7b877bed9ab02c60b
Alex Gordon <anything@fileability.net>
2012-07-10 17:32:11 +0100

    Added require stuff

--- source/chocolat.js
+++ source/chocolat.js
@@ -29,10 +29,16 @@ function load_initjs(mixinPath, mixinID, pathToInitjs) {
     var initJSSource = ...;
     
     // Load in a new context with `api` as the root
-    var vmContext = vm.runInNewContext(...)
+    var vmContext = vm.createContext(api);
+    
+    // Set up require.js in vmContext
+    vm.runInContext("var nodeRequire = require; var requireJS = nodeRequire(\"r.js\"); requireJS.config({ nodeRequire: nodeRequire, baseUrl: mixinPath }); require = function() { try { return nodeRequire.apply({}, arguments); } catch (e) { return requireJS.apply({}, arguments); } } ");
     
     // Add our new Mixin
     var mixin = new Mixin(mixinPath, mixinID, vmContext);
     loadedMixins.push(mixin);
+    
+    // Run init.js
+    vm.runInContext(initJSSource, vmContext, pathToInitjs)
 }
 */
