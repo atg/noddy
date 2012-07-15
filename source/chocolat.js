befde0412ed9a0c9f03eb037ea4da97bf486f3b1
Alex Gordon <anything@fileability.net>
2012-07-15 21:09:45 +0100

    Fixed maybe?

--- source/chocolat.js
+++ source/chocolat.js
@@ -15,14 +15,28 @@ process.on('uncaughtException', function () {
 });
 */
 
+var _ = require('underscore.js');
 var api = require('api.js');
 var loadedMixins = [];
 
 function Mixin(mixinPath, mixinID, vmContext) {
     this.path = mixinPath;
     this.vmCtx = vmContext;
-    this.id = mixinid;
+    this.id = mixinID;
 }
+
+
+/*function clone(obj){
+    if(obj == null || typeof(obj) != 'object')
+        return obj;
+    
+    var temp = obj.constructor(); // changed
+    
+    for(var key in obj)
+        temp[key] = clone(obj[key]);
+    return temp;
+}
+*/
 
 global.sayHelloTo = function(person) {
     console.log("Hello, " + person + "!");
@@ -35,26 +49,46 @@ global.load_initjs = function(mixinPath, mixinID) {
     loadedMixins = loadedMixins.filter(function (aMixin) { return a.Min.path === mixinPath; });
     
     // Read the source from disk
-    var initJSSource = fs.readFileSync(path.join(mixinPath, "init.js"));
+    var pathToInitjs = path.join(mixinPath, "init.js");
+    var initJSSource = fs.readFileSync(pathToInitjs);
     
     // Load in a new context with `api` as the root
-    var vmContext = vm.createContext(api);
+    var sandbox = _.clone(global);
+//    console.log(sandbox.require);
     
+    sandbox.mixinPath = mixinPath;
+    sandbox.nodeRequire = sandbox.require;
+    sandbox.requireJS = require("r.js");
+    sandbox.requireJS.config({
+        'nodeRequire': sandbox.nodeRequire, 'baseUrl': sandbox.mixinPath
+    });
+    sandbox.require = function() {
+        try {
+            return sandbox.nodeRequire.apply({}, arguments);
+        }
+        catch (e) {
+            return sandbox.requireJS.apply({}, arguments);
+        }
+    };
+    var vmContext = vm.createContext(sandbox);
+    
+    /*
     // Set up require.js in vmContext
     vm.runInContext(
-        "var nodeRequire = require; " +
-        "var requireJS = nodeRequire(\"r.js\"); " +
-        "requireJS.config({ " +
-            "nodeRequire: nodeRequire, baseUrl: mixinPath " +
+        "global.nodeRequire = global.require; " +
+        "global.requireJS = global.nodeRequire(\"r.js\"); " + 
+        "global.requireJS.config({ " +
+            "'nodeRequire': global.nodeRequire, 'baseUrl': global.mixinPath " +
         "}); " +
-        "require = function() { " +
+        "global.require = function() { " +
             "try { " +
-                "return nodeRequire.apply({}, arguments); " +
+                "return global.nodeRequire.apply({}, arguments); " +
             "} " +
             "catch (e) { " +
-                "return requireJS.apply({}, arguments); " +
+                "return global.requireJS.apply({}, arguments); " +
             "} " +
         "} ", vmContext);
+    */
     
     // Add our new Mixin
     var mixin = new Mixin(mixinPath, mixinID, vmContext);
