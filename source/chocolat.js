128cc4a372789fc1bfa0a1493ede5d0ad8b97283
Alex Gordon <anything@fileability.net>
2012-07-15 18:10:57 +0100

    pushing changes to chocolat js'

--- source/chocolat.js
+++ source/chocolat.js
@@ -1,8 +1,20 @@
 var os = require('os');
+var vm = require('vm');
+var fs = require('fs');
+var path = require('path');
+
 console.log("Bite my shiny metal ass [" + os.platform() + " " + os.release() + "]");
 
+// Temporary way to stop node.js quitting immediately until we add an events.EventEmitter
+process.stdin.resume();
+
 /*
-var vm = require('vm');
+ TODO: Handle uncaught exceptions
+process.on('uncaughtException', function () {
+    
+});
+*/
+
 var api = require('api.js');
 var loadedMixins = [];
 
@@ -16,22 +28,29 @@ function load_initjs(mixinPath, mixinID, pathToInitjs) {
     
     // Look in loadedMixins for this mixin
     // Remove any that match mixinPath
-    for (...) {
-        if (mixin.path === mixinPath) {
-            // Remove
-            ...
-            break;
-        }
-    }
+    loadedMixins = loadedMixins.filter(function (aMixin) { return a.Min.path === mixinPath; });
     
     // Read the source from disk
-    var initJSSource = ...;
+    var initJSSource = fs.readFileSync(path.join(mixinPath, "init.js"));
     
     // Load in a new context with `api` as the root
     var vmContext = vm.createContext(api);
     
     // Set up require.js in vmContext
-    vm.runInContext("var nodeRequire = require; var requireJS = nodeRequire(\"r.js\"); requireJS.config({ nodeRequire: nodeRequire, baseUrl: mixinPath }); require = function() { try { return nodeRequire.apply({}, arguments); } catch (e) { return requireJS.apply({}, arguments); } } ");
+    vm.runInContext(
+        "var nodeRequire = require; " +
+        "var requireJS = nodeRequire(\"r.js\"); " +
+        "requireJS.config({ " +
+            "nodeRequire: nodeRequire, baseUrl: mixinPath " +
+        "}); " +
+        "require = function() { " +
+            "try { " +
+                "return nodeRequire.apply({}, arguments); " +
+            "} " +
+            "catch (e) { " +
+                "return requireJS.apply({}, arguments); " +
+            "} " +
+        "} ");
     
     // Add our new Mixin
     var mixin = new Mixin(mixinPath, mixinID, vmContext);
@@ -40,4 +59,3 @@ function load_initjs(mixinPath, mixinID, pathToInitjs) {
     // Run init.js
     vm.runInContext(initJSSource, vmContext, pathToInitjs)
 }
-*/
