110c5437c18fbcc243837318967c480c8e98e621
Jean-Nicolas Jolivet <support@silvercocoa.com>
2012-07-19 12:20:19 -0400

    Stubbed and documented Tab

--- api/workspace.js
+++ api/workspace.js
@@ -1,5 +1,8 @@
 // MainWindow, Tab, Document and Editor
 
+/**
+ * @api private
+ */
 var MainWindow = function(nid) {
   this.nid = nid;
 };
@@ -55,4 +58,83 @@ MainWindow.prototype.sendMessage = function(selector, arguments) {
  */
 MainWindow.prototype.storage = function() {
   
+};
+
+/**
+ * @api private
+ */
+var Tab = function(nid) {
+  this.nid = nid;
+};
+
+global.Tab = Tab;
+
+/**
+ * Class method that returns the current, active tab.
+ *
+ * @return {Tab} the active tab.
+ * @memberOf Tab
+ */
+Tab.current = function() {
+  
+};
+
+/**
+ * The window which the tab is part of
+ *
+ * @return {MainWindow} the parent MainWindow.
+ * @memberOf Tab
+ */
+Tab.prototype.window = function() {
+  
+};
+
+/**
+* Get a list of all visible editors in the tab.
+*
+* @return {Array} an array of visible `Editor`s.
+* @memberOf Tab
+*/
+Tab.prototype.editors = function() {
+  
+};
+
+/**
+* Get the active editor in this tab.
+*
+* @return {Editor} the current editor.
+* @memberOf Tab
+*/
+Tab.prototype.currentEditor = function() {
+  
+};
+
+/**
+* Get a list of all the documents in the "Active" part of the sidebar.
+*
+* @return {Array} an array containing the active `Document`s
+* @memberOf Tab
+*/
+Tab.prototype.activeDocuments = function() {
+  
+};
+
+/**
+* A list of all the `Document`s currently visible in this tab.
+*
+* @return {Array} an array containing the visible `Document`s
+* @memberOf Tab
+*/
+Tab.prototype.visibleDocuments = function() {
+  
+};
+
+/**
+ * Access the storage object of the Tab (see Storage class).
+ *
+ * @return {Storage} the storage.
+ * @memberOf Tab
+ */
+Tab.prototype.storage = function() {
+
 };
